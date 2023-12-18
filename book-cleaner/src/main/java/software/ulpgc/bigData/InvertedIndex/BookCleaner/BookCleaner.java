e6805373c08627898ce031da2b3d5978cf10caec
Alejeglez <93945564+Alejeglez@users.noreply.github.com>
2023-12-18 22:46:29 +0000

    Delete book-cleaner directory

--- book-cleaner/src/main/java/software/ulpgc/bigData/InvertedIndex/BookCleaner/BookCleaner.java
+++
@@ -1,9 +0,0 @@
-package software.ulpgc.bigData.InvertedIndex.BookCleaner;
-
-import java.util.List;
-
-public interface BookCleaner {
-    String cleanText(String content);
-
-    List<String> readWords(String path);
-}
