f8744419c1bd6cb9250ed3b027e1102b68684295
Alejeglez <93945564+Alejeglez@users.noreply.github.com>
2023-12-18 22:46:37 +0000

    Delete crawler directory

--- crawler/src/main/java/software/ulpgc/bigData/InvertedIndex/crawler/BrokerCommunicator.java
+++
@@ -1,9 +0,0 @@
-package software.ulpgc.bigData.InvertedIndex.crawler;
-
-import javax.jms.Connection;
-import javax.jms.JMSException;
-
-public interface BrokerCommunicator {
-    void sendMessageToQueue(Connection connection, String message)  throws JMSException;
-    Connection createConnection() throws JMSException;
-}
--- crawler/src/main/java/software/ulpgc/bigData/InvertedIndex/crawler/Main.java
+++
@@ -1,8 +0,0 @@
-package software.ulpgc.bigData.InvertedIndex.crawler;
-
-public class Main {
-    public static void main(String[] args) {
-        Controller controller = new Controller(10);
-        controller.run();
-    }
-}
